694e85143dc607bcd16c62728584865de3fa9300
Matheus H <matheus110903@gmail.com>
2023-09-02 20:58:55 -0300

    :sparkles: feat:add Page About

---
+++ src/Pages/About/index.jsx
@@ -0,0 +1,20 @@
+import { Button } from "../../Components/Button";
+
+export const About = () => (
+  <main className="text-white m-auto max-w-4xl  h-[90vh]">
+    <div className="px-8 py-12">
+      <div>
+        <h1 className="text-4xl font-bold">About Us</h1>
+        <p className="text-xl my-6">
+          I'm Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, error. Excepturi dolor placeat porro
+          officiis! Nobis aliquam a omnis labore odit dignissimos? Libero, distinctio possimus. Assumenda error
+          architecto commodi quod. Nam rerum, unde voluptates sint assumenda repellat, quibusdam libero veniam quas
+          placeat ipsum quaerat repellendus. Mollitia iure in saepe eaque porro, vero consequatur, sed, illo quos et
+          assumenda? Pariatur, earum Son!
+        </p>
+      </div>
+
+      <Button />
+    </div>
+  </main>
+);
