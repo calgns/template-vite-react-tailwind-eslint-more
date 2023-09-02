6cbb2e3b6f4723f5432c030c92657552227c1241
Matheus H <matheus110903@gmail.com>
2023-09-02 20:58:08 -0300

    :sparkles: feat:add NotFoundPage

---
+++ src/Pages/Lost/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Lost = () => (
+  <div>
+    <h2>Are You Lost?</h2>
+    <h3>Page Not Found</h3>
+    <Link to="..">Click To Go Back</Link>
+  </div>
+);
